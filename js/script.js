e8f3ccad9143b72279f8c42de29840695a1d6804
Guz117 <cristian.guzman117@gmail.com>
2021-12-21 11:36:40 +0100

    background active

--- js/script.js
+++ js/script.js
@@ -6,11 +6,19 @@ Replica della grafica con la possibilità di avere messaggi scritti dall’utent
 
 Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, 
 visualizzare nome e immagine di ogni contatto
+
+Milestone 2
+
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare 
+tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+
+Click sul contatto mostra la conversazione del contatto cliccato
 */
 
 var app = new Vue({
     el: '#app',
     data: {
+      active: 0,
         contacts: [
             {
               name: "Michele",
@@ -97,5 +105,6 @@ var app = new Vue({
               ],
             },
           ]
-    }
+    },
+    
   })
