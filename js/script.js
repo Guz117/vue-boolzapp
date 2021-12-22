07fb0b62e7d6d16b7a587584277facd257a15971
Guz117 <cristian.guzman117@gmail.com>
2021-12-22 12:37:41 +0100

    inserimento messaggio più risposta

--- js/script.js
+++ js/script.js
@@ -13,6 +13,13 @@ Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare
 tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 
 Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando 
+“enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente 
+riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */
 
 var app = new Vue({
@@ -120,17 +127,15 @@ var app = new Vue({
             }
             this.contacts[this.active].messages.push(message);
             this.newMessage = ''
-            setTimeout (appear, 1000);
-            function appear () {
-              let messageOk = 'Ok'
-              console.log(messageOk)
-              // {
-              //   date: "10/01/2020 15:50:00",
-              //   text: 'Ok',
-              //   status: "sent"
-              // }
-              // this.contacts[this.active].messages.push(messageOk);
-            }
+            setTimeout (() =>  {
+              let messageOk = {
+                date: "10/01/2020 15:50:00",
+                text: 'Ok',
+                status: "sent"
+              }
+              this.contacts[this.active].messages.push(messageOk);
+              console.log(this)
+            }, 1000)
         }
       },
     }    
